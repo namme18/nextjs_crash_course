68f491ac8e22fbbf071ff67e0c87367407531356
namme18 <nammeselarom@gmail.com>
2022-01-07 17:30:09 +0800

    finish getStaticProps

--- pages/about.js
+++ pages/about.js
@@ -8,7 +8,7 @@ const about = () => {
             <Head>
                 <title>About</title>
             </Head>
-            <Typography variant='h1' color='textSecondary'>About Me</Typography>
+            <Typography variant='h1' color='textSecondary'>About</Typography>
         </div>
     )
 }
---
+++ pages/article/[id]/index.js
@@ -0,0 +1,44 @@
+import { useRouter } from 'next/router'
+import Link from 'next/link';
+
+function article({ article }) {
+
+    // const router = useRouter();
+    // const { id } = router.query;
+    // console.log(router);
+
+    return (
+        <>
+            <h1>{article.title}</h1>
+            <p>{article.body}</p>
+            <br />
+            <Link href='/'>Go Back</Link>
+        </>
+    )
+}
+
+export const getStaticProps = async (context) => {
+    const res = await fetch(`http://jsonplaceholder.typicode.com/posts/${context.params.id}`);
+    const article = await res.json();
+
+    return {
+        props: {
+            article
+        }
+    }
+}
+
+export const getStaticPaths = async () => {
+    const res = await fetch(`http://jsonplaceholder.typicode.com/posts`);
+    const articles = await res.json();
+
+    const ids = articles.map(article => article.id);
+
+    const paths = ids.map(id => ({params: {id: id.toString()}}))
+    return {
+            paths,
+            fallback: false
+    }
+}
+
+export default article;
